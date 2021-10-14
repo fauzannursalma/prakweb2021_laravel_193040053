5b3621198fe82f40be2a114f73882913d4693054
Fauzan Nursalma <55134999+fauzannursalma@users.noreply.github.com>
2021-10-14 08:16:32 +0700

    Menyelesaikan video Laravel 8 #5 Model, Collection & Controller

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,6 +4,7 @@
   <h1>Halaman About</h1>
   <h3>{{ $name }}</h3>
   <p>{{ $email }}</p>
+  
   <img src="img/{{ $image }}" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">    
 @endsection
     

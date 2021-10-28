4c244a7bc4ac5edcb02f6ce55773790ffd108a60
Fauzan Nursalma <55134999+fauzannursalma@users.noreply.github.com>
2021-10-28 19:38:00 +0700

    Menyelesaikan video Laravel 8 #14 View Login & Registration

---
+++ resources/views/login/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="row justify-content-center">
+  <div class="col-md-4">
+    <main class="form-signin"> 
+        <i style="font-size: 6rem;"class="bi bi-person-circle fa-10x text-danger d-block text-center"></i>
+  
+      <h1 class="h3 mb-3 fw-normal text-center">Please Log in</h1>
+      <form>
+        <div class="form-floating">
+          <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+          <label for="floatingInput">Email address</label>
+        </div>
+        <div class="form-floating">
+          <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+          <label for="floatingPassword">Password</label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Login</button>
+      </form>
+      <small class="d-block text-center mt-3">Not registered? <a href="/register">Register now!</a></small>
+    </main>
+  </div>
+</div>
+
+    
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\PostController;
 use App\Models\Category;
-use App\Models\Post;
-use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 
 
 /*
@@ -51,4 +51,7 @@ Route::get('/categories', function(){
     ]);
 });
 
+Route::get('/login', [LoginController::class, 'index']);
+
+Route::get('/register', [RegisterController::class, 'index']);
 
